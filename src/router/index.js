3464f2ac4f9dac55fee1184f8ac0c1de34fc47c5
Андрей Андронов <andrew270504cska@gmail.com>
2024-11-07 01:12:48 +0300

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,36 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHashHistory } from "vue-router"
 import homeLanding from "../pages/homeLanding.vue"
 
 const routes = [
    {
-      path: 'home',
+      path: '/home',
       name: 'home-landing',
       component: homeLanding
    },
    {
-      path: 'faq',
+      path: '/faq',
       name: 'faq-page',
       component: () => import("../pages/faqPage.vue")
    },
    {
-      path: 'privacy-policy',
+      path: '/privacy-policy',
       name: 'privacy-policy-page',
       component: () => import("../pages/privacyPolicy.vue")
    },   
    {
-      path: 'terms-and-conditions',
+      path: '/terms-and-conditions',
       name: 'terms-and-conditions-page',
       component: () => import("../pages/termsPage.vue")
    }
 ]
 
-routes.map(route => {
-   route.path = '/filter/' + route.path
-})
+// routes.map(route => {
+//    route.path = '/filter/' + route.path
+// })
 
 const router = createRouter({
    routes,
-   history: createWebHistory()
+   history: createWebHashHistory()
 })
 
 export default router;
